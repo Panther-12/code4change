510965b5a11f7cc8abe86a3da5dc8141a778ebfd
nimrod <nimrodwalwe@gmail.com>
2021-12-30 02:08:47 +0300

    value proposition

--- src/Homepage.js
+++ src/Homepage.js
@@ -21,7 +21,7 @@ const Landingpage = () =>{
                     <p id="site-name">CODE4CHANGE</p>
                 </div>
                 <div className="info-wrapper">
-
+                    <p>Code4change is a site aimed at teaching coding skills as a way of advocating for technological advancements in african society. It is a personal initiative to equip african youths with knowledge on the fast growing computing industry. The fields offered include: React frontend, node js, express backend, flask backend, python programming, html and css. The tutorials are topic based and descriptive enough to allow newbies to grasp and understand programming principles easily. If where do I start? And what do I begin with? Are your questions, then this is the right place for you. </p>
                 </div>
             </div>
         </div>
